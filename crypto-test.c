ffe7cdfa3b06570aa87c99419ef0e6425353e311
Refone <hustliuruifeng@gmail.com>
2017-05-18 21:29:30 +0800

    ctr bug debugged. speed test corret.

--- crypto-test.c
+++ crypto-test.c
@@ -4,8 +4,8 @@
 #include <asm/crypto/aes.h>
 #include "crypto.h"
 
-#define PAGE_LEN 2048
-#define AES_METHOD_CTR
+#define PAGE_LEN 2097152
+//#define AES_METHOD_CTR
 //#define AES_METHOD_CBC
 //#define AES_METHOD_ECB
 
@@ -23,15 +23,18 @@ static const unsigned char KEY[] = {0x2b,0x7e,0x15,0x16,0x28,0xae,0xd2,0xa6,
 //static const unsigned char KEY[] = {0x7E,0x24,0x06,0x78,0x17,0xFA,0xE0,0xD7,
 //        0x43,0xD6,0xCE,0x1F,0x32,0x53,0x91,0x63};
 
-//static const unsigned char IV[] = {0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,
-//									0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f};
+static const unsigned char IV[] = {0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,
+									0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f};
 
-static const unsigned char IV[] = {0xC0,0x54,0x3B,0x59,0xDA,0x48,0xD9,0x0B};
+//static const unsigned char IV[] = {0xC0,0x54,0x3B,0x59,0xDA,0x48,0xD9,0x0B};
 
 static const unsigned char NONCE[] = {0x00,0x6C,0xB6,0xDB};
 
 static struct crypto_aes_ctx _ctx;
 static struct crypto_aes_ctx *ctx = &_ctx;
+
+static unsigned long s_time, e_time;
+static unsigned long vpage2, vpage1;
 
 extern void AES_CBC_encrypt (const unsigned char *in,
 							unsigned char *out,
@@ -113,18 +116,15 @@ void print_text(uint8_t* src, unsigned long length)
 
 static int init_crypto(void)
 {
-	unsigned long s_time, e_time;
-	unsigned long vpage1, vpage2;
-	ALIGN16 uint8_t TEST_PLAIN[] = 
-	   {0x6b,0xc1,0xbe,0xe2,0x2e,0x40,0x9f,0x96,
-		0xe9,0x3d,0x7e,0x11,0x73,0x93,0x17,0x2a,
-		0xae,0x2d,0x8a,0x57,0x1e,0x03,0xac,0x9c,
-		0x9e,0xb7,0x6f,0xac,0x45,0xaf,0x8e,0x51,
-		0x30,0xc8,0x1c,0x46,0xa3,0x5c,0xe4,0x11,
-		0xe5,0xfb,0xc1,0x19,0x1a,0x0a,0x52,0xef,
-		0xf6,0x9f,0x24,0x45,0xdf,0x4f,0x9b,0x17,
-		0xad,0x2b,0x41,0x7b,0xe6,0x6c,0x37,0x10};
-
+//	ALIGN16 uint8_t TEST_PLAIN[] = 
+//	   {0x6b,0xc1,0xbe,0xe2,0x2e,0x40,0x9f,0x96,
+//		0xe9,0x3d,0x7e,0x11,0x73,0x93,0x17,0x2a,
+//		0xae,0x2d,0x8a,0x57,0x1e,0x03,0xac,0x9c,
+//		0x9e,0xb7,0x6f,0xac,0x45,0xaf,0x8e,0x51,
+//		0x30,0xc8,0x1c,0x46,0xa3,0x5c,0xe4,0x11,
+//		0xe5,0xfb,0xc1,0x19,0x1a,0x0a,0x52,0xef,
+//		0xf6,0x9f,0x24,0x45,0xdf,0x4f,0x9b,0x17,
+//		0xad,0x2b,0x41,0x7b,0xe6,0x6c,0x37,0x10};
 //unsigned char TEST_PLAIN[] = {0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,
 //						0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F,
 //						0x10,0x11,0x12,0x13,0x14,0x15,0x16,0x17,
@@ -145,16 +145,23 @@ static int init_crypto(void)
 //		0x3f,0xf1,0xca,0xa1,0x68,0x1f,0xac,0x09,
 //		0x12,0x0e,0xca,0x30,0x75,0x86,0xe1,0xa7};
 
-	ALIGN16 uint8_t VECTOR[64];
+//	ALIGN16 uint8_t VECTOR[64];
+
+
+	crypto_aes_expand_key(ctx, KEY, AES_KEYSIZE_128);
 
 	printk("crypto module init.\n");
+	
 	vpage1 = __get_free_pages(GFP_KERNEL, 9);
 	printk("vpage1: %lx\n", vpage1);
 	vpage2 = __get_free_pages(GFP_KERNEL, 9);
 	printk("vpage2: %lx\n", vpage2);
-
-	crypto_aes_expand_key(ctx, KEY, AES_KEYSIZE_128);
-
+//	a = vpage1;
+//	b = vpage2;
+//	printk("a: %lx\n", a);
+//	printk("b: %lx\n", b);
+//	printk("TEST_PLAIN: %p\n", TEST_PLAIN);
+	
 /************* crypto_aes  *****************/
 //	printk("TEST_PLAIN:\n");
 //	print_text(TEST_PLAIN, 64);
@@ -167,17 +174,32 @@ static int init_crypto(void)
 /************ crypto_aes end ***************/
 
 /************* AESENC TEST *****************/
-	memcpy(VECTOR, TEST_PLAIN, 64);
-	printk("TEST_PLAIN:\n");
-	print_text(TEST_PLAIN, 64);
-	aesenc_encrypt(TEST_PLAIN, VECTOR, 16);
-	printk("TEST_CIPHER:\n");
-	print_text(VECTOR, 64);
+//	memcpy(VECTOR, TEST_PLAIN, 64);
+//	printk("vpage1: %lx\n", vpage1);
+//	printk("vpage1: %x\n", (int *)vpage1);
+//	printk("vpage2: %lx\n", vpage2);
+//	printk("TEST_PLAIN:\n");
+//	printk("a: %lx\n", a);
+//	printk("b: %lx\n", b);
+	
+//	print_text(TEST_PLAIN, 64);
+//	aesenc_encrypt(TEST_PLAIN, VECTOR, 16);
+//	printk("a: %lx\n", a);
+//	printk("b: %lx\n", b);
+//	printk("vpage1: %lx\n", vpage1);
+//	printk("vpage1: %x\n", (int *)vpage1);
+//	printk("vpage2: %lx\n", vpage2);
+//	printk("TEST_CIPHER:\n");
+//	print_text(VECTOR, 64);
 //	printk("CTR128_EXPECTED:\n");
 //	print_text(CTR128_EXPECTED, 32);
-	aesenc_decrypt(VECTOR, VECTOR, 16);
-	printk("TEST_DECRYPT:\n");
-	print_text(VECTOR, 64);
+//	aesenc_decrypt(VECTOR, VECTOR, 16);
+//	printk("a: %lx\n", a);
+//	printk("b: %lx\n", b);
+//	printk("vpage1: %lx\n", vpage1);
+//	printk("vpage2: %lx\n", vpage2);
+//	printk("TEST_DECRYPT:\n");
+//	print_text(VECTOR, 64);
 /************* AESENC TEST END *************/
 
 /***************************************************************
@@ -195,16 +217,16 @@ AESENC(hardware) Encryption:
 	printk("vpage1: %lx\n", vpage1);
 	printk("vpage2: %lx\n", vpage2);
 
-	if (!(vpage1 & vpage2)) {
+	if (!vpage1 || !vpage2) {
 		pr_err("crypto-test: Could not get free pages\n");
         return 0;
 	}
-	
+
 	rdtscll(s_time);
 	memcpy((unsigned char *)vpage1, (unsigned char *)vpage2, PAGE_LEN);
 	rdtscll(e_time);
 	printk("crypto-test: memcpy time: %ld cycles.\n", e_time - s_time);
-	
+/*	
 	rdtscll(s_time);
 	aes_encrypt((unsigned char *)vpage1, (unsigned char *)vpage2, PAGE_LEN);
 	rdtscll(e_time);
@@ -214,20 +236,46 @@ AESENC(hardware) Encryption:
 	aes_decrypt((unsigned char *)vpage1, (unsigned char *)vpage2, PAGE_LEN);
 	rdtscll(e_time);
 	printk("crypto-test: aes_dec time: %ld cycles.\n", e_time - s_time);
-	
-	rdtscll(s_time);
-	aesenc_encrypt((unsigned char *)vpage1, (unsigned char *)vpage2, PAGE_LEN);
-	rdtscll(e_time);
-	printk("crypto-test: aesenc_enc time: %ld - %ld = %ld cycles.\n", e_time, s_time, e_time - s_time);
+*/
 
 	rdtscll(s_time);
-	aesenc_decrypt((unsigned char *)vpage1, (unsigned char *)vpage2, PAGE_LEN);
+	aesni_ecb_enc(ctx, (unsigned char *)vpage2, (unsigned char *)vpage1, PAGE_LEN);
 	rdtscll(e_time);
-	printk("crypto-test: aesenc_dec time: %ld - %ld = %ld cycles.\n", e_time, s_time, e_time - s_time);
+	printk("crypto-test: ecb time: %ld cycles.\n", e_time - s_time);
+	
+	rdtscll(s_time);
+	aesni_ecb_dec(ctx, (unsigned char *)vpage1, (unsigned char *)vpage2, PAGE_LEN);
+	rdtscll(e_time);
+	printk("crypto-test: ecb time: %ld cycles.\n", e_time - s_time);
+	
+	rdtscll(s_time);
+	AES_CBC_encrypt((unsigned char*)vpage1, (unsigned char*)vpage2, 
+					IV, PAGE_LEN, (unsigned char *)ctx->key_enc, 10);
+	rdtscll(e_time);
+	printk("crypto-test: cbc_enc time: %ld - %ld = %ld cycles.\n", e_time, s_time, e_time - s_time);
+	
+	rdtscll(s_time);
+	AES_CBC_decrypt((unsigned char*)vpage1, (unsigned char*)vpage2, 
+					IV, PAGE_LEN, (unsigned char *)ctx->key_enc, 10);
+	rdtscll(e_time);
+	printk("crypto-test: cbc_dec time: %ld - %ld = %ld cycles.\n", e_time, s_time, e_time - s_time);
+	
+	rdtscll(s_time);
+	AES_CTR_encrypt((unsigned char*)vpage1, (unsigned char*)vpage2,
+					IV, NONCE, PAGE_LEN, (unsigned char *)ctx->key_enc, 10);
+	rdtscll(e_time);
+	printk("crypto-test: ctr time: %ld - %ld = %ld cycles.\n", e_time, s_time, e_time - s_time);
+
+	rdtscll(s_time);
+	AES_CTR_encrypt((unsigned char*)vpage1, (unsigned char*)vpage2,
+					IV, NONCE, PAGE_LEN, (unsigned char *)ctx->key_enc, 10);
+	rdtscll(e_time);
+	printk("crypto-test: ctr time: %ld - %ld = %ld cycles.\n", e_time, s_time, e_time - s_time);
 
 	free_pages(vpage1, 9);
 	free_pages(vpage2, 9);
 
+	printk(KERN_INFO "crypto-test.ko: init finished.");
 	return 0;
 }
 
